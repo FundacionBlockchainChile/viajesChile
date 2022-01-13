7da7ffc25051ad5a7484ad8fc9f99b39a30075af
Sergio <38168163+FundacionBlockchainChile@users.noreply.github.com>
2022-01-13 00:14:13 -0300

    first commit - working page

---
+++ js/script.js
@@ -0,0 +1,63 @@
+$(function () {
+  ;('use strict')
+
+  $('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide')
+  })
+
+  // let $navbarNav = $('.hide_menu')
+  // let $inicio_btn = $('#inicio_btn')
+
+  // let $btn_one = $('#button_one')
+  // let $btn_two = $('#button_two')
+  // let $btn_three = $('#button_three')
+  let $modal = $('.my_modal')
+  // let $open_modal_btn = $('.open_modal')
+  // let $close_modaL_btn = $('.close_modal')
+  // let $check_email_btn = $('.check_email')
+  // let $input_email = $('#input_email')
+
+  $('.desc_one').toggle()
+  $('.desc_two').toggle()
+  $('.desc_three').toggle()
+  $modal.toggle()
+
+  // // $inicio_btn.on('click', () => {
+  // //   $navbarNav.toggleClass('show')
+  // // })
+
+  // $btn_one.on('click', () => {
+  //   $('.desc_one').toggle()
+  //   $btn_one.toggleClass('fa-arrow-alt-circle-down fa-arrow-alt-circle-up')
+  // })
+
+  // $btn_two.on('click', () => {
+  //   $('.desc_two').toggle()
+  //   $btn_two.toggleClass('fa-arrow-alt-circle-down fa-arrow-alt-circle-up')
+  // })
+
+  // $btn_three.on('click', () => {
+  //   $('.desc_three').toggle()
+  //   $btn_three.toggleClass('fa-arrow-alt-circle-down fa-arrow-alt-circle-up')
+  // })
+
+  // $open_modal_btn.on('click', () => {
+  //   $('.my_modal').toggle()
+  // })
+
+  // $close_modaL_btn.on('click', () => {
+  //   $('.my_modal').toggle()
+  //   $input_email.val('')
+  // })
+
+  // $check_email_btn.on('click', () => {
+  //   var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i
+  //   if (testEmail.test($input_email.val())) {
+  //     alert(`Your email was sended. Thanks!`)
+  //     $('.my_modal').toggle()
+  //     $input_email.val('')
+  //   } else {
+  //     alert(`Please enter a valid email`)
+  //   }
+  // })
+})
