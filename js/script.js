5769d51efe34b844ab96b5c079c4815219d308a7
Sergio <38168163+FundacionBlockchainChile@users.noreply.github.com>
2022-01-14 00:17:06 -0300

    other style effects added

--- js/script.js
+++ js/script.js
@@ -1,82 +1,54 @@
 $(function () {
   ;('use strict')
 
+  // CONSTANTS
+  const nav = $('nav')
+  const form_btn = $('#form_btn')
+  const close_modaL_one_btn = $('.modal_one .close_modal')
+  const close_modaL_two_btn = $('.modal_two .close_modal')
+  const check_email_btn = $('.check_email')
+  const input_email = $('#input_email')
+
+  // TOOLTIP SCRIPT
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+  // HIDE DROPABLE MENU AFTER ITEM CLICK
   $('.navbar-nav>li>a').on('click', function () {
     $('.navbar-collapse').collapse('hide')
   })
 
-  let nav = $('nav')
-  let send_btn = $('#send_btn')
-  // let $inicio_btn = $('#inicio_btn')
-
-  // let $btn_one = $('#button_one')
-  // let $btn_two = $('#button_two')
-  // let $btn_three = $('#button_three')
-  let $modal = $('.my_modal')
-  // let $open_modal_btn = $('.open_modal')
-  let close_modaL_one_btn = $('.modal_one .close_modal')
-  let close_modaL_two_btn = $('.modal_two .close_modal')
-  let $check_email_btn = $('.check_email')
-  let $input_email = $('#input_email')
-
-  // $('.desc_one').toggle()
-  // $('.desc_two').toggle()
-  // $('.desc_three').toggle()
-  // $modal.toggle()
-
-  // SCROLL DETECT
+  // WINDOWS SCROLL DETECT
   $(window).scroll(function (event) {
     var scroll = $(window).scrollTop()
     if (scroll > 300) $(nav).removeClass('backTransparent')
     else $(nav).addClass('backTransparent')
   })
 
-  send_btn.click((event) => {
+  form_btn.click((event) => {
+    event.stopPropagation()
+    console.log('stoped')
+    $('.modal_one').removeClass('hide')
+    event.stopPropagation()
+  })
+
+  close_modaL_one_btn.click((event) => {
     event.stopPropagation()
     $('.modal_one').toggleClass('hide')
   })
 
-  close_modaL_one_btn.click((event) => {
-    $('.modal_one').toggleClass('hide')
-  })
-
   close_modaL_two_btn.click((event) => {
+    event.stopPropagation()
     $('.modal_two').toggleClass('hide')
   })
 
-  // // $inicio_btn.on('click', () => {
-  // //   $navbarNav.toggleClass('show')
-  // // })
-
-  // $btn_one.on('click', () => {
-  //   $('.desc_one').toggle()
-  //   $btn_one.toggleClass('fa-arrow-alt-circle-down fa-arrow-alt-circle-up')
-  // })
-
-  // $btn_two.on('click', () => {
-  //   $('.desc_two').toggle()
-  //   $btn_two.toggleClass('fa-arrow-alt-circle-down fa-arrow-alt-circle-up')
-  // })
-
-  // $btn_three.on('click', () => {
-  //   $('.desc_three').toggle()
-  //   $btn_three.toggleClass('fa-arrow-alt-circle-down fa-arrow-alt-circle-up')
-  // })
-
-  // $open_modal_btn.on('click', () => {
-  //   $('.my_modal').toggle()
-  // })
-
-  // $close_modaL_btn.on('click', () => {
-  //   $('.my_modal').toggle()
-  //   $input_email.val('')
-  // })
-
-  $check_email_btn.on('click', () => {
+  // CHECK VALID MAIL FORMAT
+  check_email_btn.on('click', () => {
     var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i
-    if (testEmail.test($input_email.val())) {
+    if (testEmail.test(input_email.val())) {
       $('.modal_one').toggleClass('hide')
-      $input_email.val('')
+      input_email.val('')
       $('.modal_two').toggleClass('hide')
     } else {
       alert(`Please enter a valid email`)
